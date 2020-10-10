3a545cd91e1425ba04ae31c255415d07e518317c
Pavel Solovey <pavel_salavei@senla.eu>
2020-10-10 12:13:57 +0300

    added i18n

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,34 @@
 import { Component } from '@angular/core';
+import {TranslateService} from '@ngx-translate/core';
 
+export const timeForRender = 30;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
 export class AppComponent {
+  isNavOpened = false;
+  translateService: TranslateService;
+  isRuTranslateOn: boolean;
+
+  constructor(translate: TranslateService) {
+    this.translateService = translate;
+    this.translateService.setDefaultLang('ru');
+    this.translateService.use('ru');
+    this.isRuTranslateOn = true;
+  }
+
+  navTo(anchor: string) {
+    this.isNavOpened = false;
+    setTimeout(() => {
+      location.href = anchor;
+    }, timeForRender);
+  }
+
+  setLocale(lang: string) {
+    this.translateService.setDefaultLang(lang);
+    this.translateService.use(lang);
+    this.isRuTranslateOn = lang === 'ru';
+  }
 }
